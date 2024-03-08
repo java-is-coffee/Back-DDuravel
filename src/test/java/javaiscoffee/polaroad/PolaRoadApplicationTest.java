05b3cc83c1861f1ff5096ade032eb8024b5277ae
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-08 15:23:39 +0900

    PO-67 cicd 설정

--- src/test/java/javaiscoffee/polaroad/PolaRoadApplicationTest.java
+++ src/test/java/javaiscoffee/polaroad/PolaRoadApplicationTest.java
@@ -2,8 +2,12 @@ package javaiscoffee.polaroad;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"jwt.secret=3123758a0d7ef02a46cba8bdd3f898dec8afc9f8470341af789d59f3695093be"
+})
 class PolaRoadApplicationTest {
 
 	@Test
