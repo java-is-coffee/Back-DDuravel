12b1ce9b5fd392f87d13ab52f8b5f2b5613647b8
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-08 21:37:56 +0900

    PO-67 도커 설정 변경

--- src/main/java/javaiscoffee/polaroad/PolaRoadApplication.java
+++ src/main/java/javaiscoffee/polaroad/PolaRoadApplication.java
@@ -1,9 +1,15 @@
 package javaiscoffee.polaroad;
 
+import javaiscoffee.polaroad.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@Import(JpaConfig.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)	//스프링 시큐리티 기본 로그인 화면 제거
+@EnableWebSecurity
 public class PolaRoadApplication {
 
 	public static void main(String[] args) {
