011435ff9c68f6855f5a430688cd157f97786e94
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-11 22:42:27 +0900

    Swagger Dto 설명 예시 추가

--- src/main/java/javaiscoffee/polaroad/login/LoginDto.java
+++ src/main/java/javaiscoffee/polaroad/login/LoginDto.java
@@ -1,13 +1,17 @@
 package javaiscoffee.polaroad.login;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javaiscoffee.polaroad.validator.EmailCheck;
 import javaiscoffee.polaroad.validator.PasswordCheck;
 import lombok.Data;
 
 @Data
+@Schema(description = "로그인 입력값을 받는 requestDto")
 public class LoginDto {
     @EmailCheck
+    @Schema(description = "## 사용자 이메일", example = "aaa@naver.com")
     private String email;
     @PasswordCheck
+    @Schema(description = "## 사용자 비밀번호", example = "AaBb1234%")
     private String password;
 }
--- src/main/java/javaiscoffee/polaroad/login/RegisterDto.java
+++ src/main/java/javaiscoffee/polaroad/login/RegisterDto.java
@@ -1,5 +1,6 @@
 package javaiscoffee.polaroad.login;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javaiscoffee.polaroad.validator.EmailCheck;
 import javaiscoffee.polaroad.validator.NicknameCheck;
 import javaiscoffee.polaroad.validator.PasswordCheck;
@@ -8,11 +9,15 @@ import lombok.Data;
 @Data
 public class RegisterDto {
     @EmailCheck
+    @Schema(description = "## 사용자 이메일", example = "aaa@naver.com")
     private String email;
 //    private String certificationNumber;
+    @Schema(description = "사용자 이름", example = "박자바")
     private String name;
     @NicknameCheck
+    @Schema(description = "사용자 닉네임", example = "자바커피")
     private String nickname;
     @PasswordCheck
+    @Schema(description = "## 사용자 비밀번호", example = "AaBb1234%")
     private String password;
 }
