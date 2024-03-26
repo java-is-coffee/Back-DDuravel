083256517cdb0f7c800085c85a91223e3a9e507b
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-26 17:43:07 +0900

    DEPLOY / 워크플로우 수정

--- src/test/java/javaiscoffee/polaroad/PolaRoadApplicationTest.java
+++ src/test/java/javaiscoffee/polaroad/PolaRoadApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-		"JWT_SECRET_KEY=3123758a0d7ef02a46cba8bdd3f898dec8afc9f8470341af789d59f3695093be12314515135ddas123123131251342131231231215124124213123123123414",
+		"JWT_SECRET_KEY=3123755132fdfds4daas4551af789d59f36977df5093be12c2314515135ddasg1f5k12hdfhjk412bh531uiadfi14b14bwebs52",
 		"KAKAO_API_KEY=tabve4120123408dse89749c4123041zvx",
 		"KAKAO_REDIRECT_URI=http://localhost:3000/explore",
 		"MAIL_PASSWORD=abcd efgh ijkl",
