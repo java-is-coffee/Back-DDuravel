cfe002637c8f50fe5f4e3cc074eb3e1670c99556
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-27 16:28:47 +0900

    Fix / 이메일 컨트롤러 수정

--- src/main/java/javaiscoffee/polaroad/login/emailAuthentication/MailSendController.java
+++ src/main/java/javaiscoffee/polaroad/login/emailAuthentication/MailSendController.java
@@ -16,18 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailSendController {
     private final MailSendService mailSendService;
 
-
+    @Operation(description = "이메일 인증 메일 전송")
+    @Parameter(name = "email", description = "설정할 이메일", required = true, example = "aaa@naver.com")
     @Parameter(name = "certificationNumber", description = "설정할 임시 패스워드", required = true, example = "12345678")
     @PostMapping("/certification")
-    public ResponseEntity<String> sendCertification(String email, @RequestParam("certificationNumber") String certificationNumber) {
+    public ResponseEntity<String> sendCertification(@RequestParam("email") String email, @RequestParam("certificationNumber") String certificationNumber) {
         mailSendService.sendCertificationEmail(email, certificationNumber);
         return ResponseEntity.ok(ResponseMessages.SUCCESS.getMessage());
     }
 
     @Operation(description = "비밀번호 재설정 메일 전송")
+    @Parameter(name = "email", description = "설정할 이메일", required = true, example = "aaa@naver.com")
     @Parameter(name = "tempPassword", description = "설정할 임시 패스워드", required = true, example = "a123123!")
     @PostMapping("/password-reset")
-    public ResponseEntity<String> sendPasswordReset(String email, @RequestParam("tempPassword") String tempPassword) {
+    public ResponseEntity<String> sendPasswordReset(@RequestParam("email") String email, @RequestParam("tempPassword") String tempPassword) {
         mailSendService.sendPasswordResetEmail(email,tempPassword);
         return ResponseEntity.ok(ResponseMessages.SUCCESS.getMessage());
     }
