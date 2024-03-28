d2ce0df1b9b14da159a8982b606f01d2f1edc75b
Park SangHyeon <tkrhkrkfn@naver.com>
2024-03-28 17:24:34 +0900

    FIX / swagger 주소 수정

--- src/main/java/javaiscoffee/polaroad/PolaRoadApplication.java
+++ src/main/java/javaiscoffee/polaroad/PolaRoadApplication.java
@@ -18,11 +18,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @Import(JpaConfig.class)
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)	//스프링 시큐리티 기본 로그인 화면 제거
-//@OpenAPIDefinition(
-//		servers = {
-//				@Server(url = "https://k951a463f2f5fa.user-app.krampoline.com", description = "Default Server url")
-//		}
-//)
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://polaroad.shop", description = "Default Server url")
+		}
+)
 @EnableWebSecurity
 public class PolaRoadApplication {
 
