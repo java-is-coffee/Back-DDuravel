81b33f77c4b5af18f7909b95eea4643c15cc52c9
roaroa <hyh1216@naver.com>
2024-03-28 14:35:05 +0900

    Fix/PO-38 앨범 api 정리
    스웨거, 검증, 예외 처리 수정했습니다

--- src/main/java/javaiscoffee/polaroad/album/AlbumService.java
+++ src/main/java/javaiscoffee/polaroad/album/AlbumService.java
@@ -50,6 +50,9 @@ public class AlbumService {
         Album newAlbum = new Album();
         BeanUtils.copyProperties(albumDto, newAlbum);
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
+        if (member.getStatus() == MemberStatus.DELETED) {
+            throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
+        }
         newAlbum.setMember(member);
         Album savedAlbum = albumRepository.save(newAlbum);
 
@@ -71,12 +74,9 @@ public class AlbumService {
      * 앨범 1개 조회
      */
     public ResponseAlbumDto getAlbum(Long memberId, Long albumId) {
-        // 앨범이 없으면 NOT_FOUND
         Album findedAlbum = albumRepository.findById(albumId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
-        // 요청한 멤버가 앨범 생성자가 아닌 경우
         if (!memberId.equals(findedAlbum.getMember().getMemberId())) throw new ForbiddenException(ResponseMessages.FORBIDDEN.getMessage());
 
         List<AlbumCard> albumCard = albumCardRepository.findAllByAlbum(findedAlbum);
@@ -87,10 +87,9 @@ public class AlbumService {
     public ResponseAlbumDto editAlbum(AlbumDto editAlbumDto, Long albumId, Long memberId) {
         Album oldAlbum = albumRepository.findById(albumId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
-        // 요청한 멤버가 앨범 생성자가 아닌 경우
         if (!memberId.equals(oldAlbum.getMember().getMemberId())) throw new ForbiddenException(ResponseMessages.FORBIDDEN.getMessage());
+
         // 수정된 내용 적용
         oldAlbum.setName(editAlbumDto.getName());
         oldAlbum.setDescription(editAlbumDto.getDescription());
@@ -107,12 +106,9 @@ public class AlbumService {
     }
 
     public ResponseEntity<String> deleteAlbum(Long memberId, Long albumId) {
-        // 삭제 요청한 앨범이 없는 경우
         Album album = albumRepository.findById(albumId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
-        // 요청한 멤버가 앨범 생성자가 아닌 경우
         if (!memberId.equals(album.getMember().getMemberId())) throw new ForbiddenException(ResponseMessages.FORBIDDEN.getMessage());
 
         // albumCard 삭제
@@ -128,14 +124,12 @@ public class AlbumService {
     public ResponseAlbumDto addAlbumCard(RequestAlbumCardDto addAlbumCardDto, Long albumId, Long memberId) {
         Album album = albumRepository.findById(albumId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
-        // 요청한 멤버가 앨범 생성자가 아닌 경우
         if (!memberId.equals(album.getMember().getMemberId())) throw new ForbiddenException(ResponseMessages.FORBIDDEN.getMessage());
+
         // 새로 추가된 앨범 카드 저장
         List<Long> cardIdList = addAlbumCardDto.getCardId();
         albumCardService.addCard(cardIdList, album);
-
         List<AlbumCardInfoDto> albumCardInfoDto = toAlbumCardInfoDto(album.getAlbumCards());
         return toResponseAlbumDto(album, albumCardInfoDto);
     }
@@ -143,14 +137,12 @@ public class AlbumService {
     public ResponseAlbumDto deleteAlbumCard(RequestAlbumCardDto deleteAlbumCardDto, Long albumId, Long memberId) {
         Album album = albumRepository.findById(albumId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
-        // 요청한 멤버가 앨범 생성자가 아닌 경우
         if (!memberId.equals(album.getMember().getMemberId())) throw new ForbiddenException(ResponseMessages.FORBIDDEN.getMessage());
+
         // 앨범에서 앨범 카드 삭제
         List<Long> cardIdList = deleteAlbumCardDto.getCardId();
         albumCardService.deleteCard(cardIdList, album);
-
         List<AlbumCardInfoDto> albumCardInfoDto = toAlbumCardInfoDto(album.getAlbumCards());
         return toResponseAlbumDto(album, albumCardInfoDto);
     }
@@ -158,7 +150,6 @@ public class AlbumService {
     public SliceAlbumListDto<AlbumInfoDto> getPagedAlbumList(int page, Long memberId) {
         page = (page == 0) ? 0 : (page - 1);
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(ResponseMessages.NOT_FOUND.getMessage()));
-        //  멤버가 없는 경우, 멤버가 삭제된 경우
         if (!member.getStatus().equals(MemberStatus.ACTIVE)) throw new NotFoundException(ResponseMessages.NOT_FOUND.getMessage());
         //HACK: 요청한 멤버가 앨범 생성자가 아닌 경우는 어떻게 할 것인지
 
