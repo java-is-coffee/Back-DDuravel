51be764eab04ebf59bb8d26a2b8c5f53916b909f
roaroa <hyh1216@naver.com>
2024-04-04 10:58:24 +0900

    Test/PO-38 테스트 작성 중 develop pull하기 위해서 커밋

--- src/main/java/javaiscoffee/polaroad/review/ReviewDto.java
+++ src/main/java/javaiscoffee/polaroad/review/ReviewDto.java
@@ -1,6 +1,7 @@
 package javaiscoffee.polaroad.review;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.util.List;
@@ -8,12 +9,18 @@ import java.util.List;
 
 @Data
 public class ReviewDto {
+    @NotNull
+    @Min(1L)
+    @Max(10L)
     @Schema(description = "## 포스트 Id", example = "1")
     private Long postId;
+    @NotNull
     @Schema(description = "## 맴버 Id", example = "1")
     private Long memberId;
     @Schema(description = "## 댓글 본문", example = "저도 다녀왔는데 너무 좋았어요.")
     private String content;
+    @NotNull
+    @Size(min = 1,max = 5)
     @Schema(description = "## 사진 url 리스트")
-    private List<String> reviewPhotoList;
+    private List<@NotBlank String> reviewPhotoList;
 }
