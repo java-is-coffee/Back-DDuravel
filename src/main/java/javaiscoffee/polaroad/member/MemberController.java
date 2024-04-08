5de3364eb50a04cd73c5bb425064bf1a9f508fe8
Jiho Yoon <j5i3h8o8@gmail.com>
2024-04-08 12:55:27 +0900

    FIX : PO-42 MemberController 클래스의 resetPassword 메소드 수정
    password 인증 방법 추가

--- src/main/java/javaiscoffee/polaroad/member/MemberController.java
+++ src/main/java/javaiscoffee/polaroad/member/MemberController.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -83,7 +84,7 @@ public class MemberController {
             @ApiResponse(responseCode = "400", description = "잘못된 요청인 경우")
     })
     @PatchMapping("/my/edit/reset-password")
-    public ResponseEntity<Boolean> resetPassword(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody RequestWrapperDto<PasswordResetRequestDto> wrapperDto) {
+    public ResponseEntity<Boolean> resetPassword(@AuthenticationPrincipal CustomUserDetails userDetails, @Validated @RequestBody RequestWrapperDto<PasswordResetRequestDto> wrapperDto) {
         String email = userDetails.getUsername();
         PasswordResetRequestDto passwordResetRequestDto = wrapperDto.getData();
         log.info("비밀번호 재설정하려는 email = {}", email);
--- src/main/java/javaiscoffee/polaroad/member/PasswordResetRequestDto.java
+++ src/main/java/javaiscoffee/polaroad/member/PasswordResetRequestDto.java
@@ -1,11 +1,14 @@
 package javaiscoffee.polaroad.member;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javaiscoffee.polaroad.validator.EmailCheck;
+import javaiscoffee.polaroad.validator.PasswordCheck;
 import lombok.Data;
 
 @Data
 @Schema(description = "비밀번호 리셋 RequestDto")
 public class PasswordResetRequestDto {
+    @PasswordCheck
     @Schema(description = "리셋하려는 비밀번호", example = "a123123!")
     private String password;
 }
