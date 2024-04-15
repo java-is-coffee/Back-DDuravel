e0b7f4981a17e450e3fcda14eb59295db31c092e
Park SangHyeon <tkrhkrkfn@naver.com>
2024-04-15 21:42:21 +0900

    FIX / PO-32 QueryCardRepositoryImpl sql 쿼리문 수정

--- src/main/java/javaiscoffee/polaroad/post/card/Card.java
+++ src/main/java/javaiscoffee/polaroad/post/card/Card.java
@@ -22,6 +22,23 @@ import java.util.List;
 )
 @Builder
 @ToString
+@SqlResultSetMapping(
+        name = "MapCardListDtoMapping",
+        classes = {
+                @ConstructorResult(
+                        targetClass = MapCardListDto.class,
+                        columns = {
+                                @ColumnResult(name = "post_id", type = Long.class),
+                                @ColumnResult(name = "card_id", type = Long.class),
+                                @ColumnResult(name = "image", type = String.class),
+                                @ColumnResult(name = "content", type = String.class),
+                                @ColumnResult(name = "location", type = String.class),
+                                @ColumnResult(name = "latitude", type = Double.class),
+                                @ColumnResult(name = "longitude", type = Double.class)
+                        }
+                )
+        }
+)
 public class Card {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cardId;
--- src/main/java/javaiscoffee/polaroad/post/card/MapCardListDto.java
+++ src/main/java/javaiscoffee/polaroad/post/card/MapCardListDto.java
@@ -10,23 +10,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@SqlResultSetMapping(
-        name = "MapCardListDtoMapping",
-        classes = {
-                @ConstructorResult(
-                        targetClass = MapCardListDto.class,
-                        columns = {
-                                @ColumnResult(name = "post_id", type = Long.class),
-                                @ColumnResult(name = "card_id", type = Long.class),
-                                @ColumnResult(name = "image", type = String.class),
-                                @ColumnResult(name = "content", type = String.class),
-                                @ColumnResult(name = "location", type = String.class),
-                                @ColumnResult(name = "latitude", type = Double.class),
-                                @ColumnResult(name = "longitude", type = Double.class)
-                        }
-                )
-        }
-)
 public class MapCardListDto {
     private Long postId;
     private Long cardId;
