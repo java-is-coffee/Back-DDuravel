b91985f869a07da055abe3818c1e7aa6904207be
Park SangHyeon <tkrhkrkfn@naver.com>
2024-04-15 12:49:31 +0900

    FIX / CardRepository 지도에서 카드 조회 api 수정
    - QueryCardRepositoryImpl : 지도에서 카드 조회시 테이블 이름이 잘못된 부분 수정

--- src/main/java/javaiscoffee/polaroad/post/card/QueryCardRepositoryImpl.java
+++ src/main/java/javaiscoffee/polaroad/post/card/QueryCardRepositoryImpl.java
@@ -25,12 +25,12 @@ public class QueryCardRepositoryImpl implements QueryCardRepository{
     public List<MapCardListDto> getMapCardListByKeyword(String searchKeyword, PostConcept concept, double swLatitude, double neLatitude, double swLongitude, double neLongitude, int pageSize) {
         // 기본 쿼리 구성
         String sql = "SELECT c.post_id, c.card_id, c.image, c.content, c.location, c.latitude, c.longitude " +
-                "FROM Cards c " +
-                "JOIN Posts p ON c.post_id = p.id ";
+                "FROM cards c " +
+                "JOIN posts p ON c.post_id = p.id ";
 
         // 검색어가 있을 경우 멤버와 조인
         if (searchKeyword != null && !searchKeyword.isEmpty()) {
-            sql += "JOIN Member m ON c.member_id = m.id ";
+            sql += "JOIN member m ON c.member_id = m.id ";
         }
 
         // 위치 기반 검색 조건 추가
