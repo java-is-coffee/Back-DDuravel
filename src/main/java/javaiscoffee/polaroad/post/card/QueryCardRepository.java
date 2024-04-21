2f2eb674046700d2c88e59e996891e8d9de94543
Park SangHyeon <tkrhkrkfn@naver.com>
2024-04-21 18:04:01 +0900

    FIX / PO-32 지도에서 카드 조회할 때 포스트 삭제된 카드가 조회되는 오류 수정

--- src/main/java/javaiscoffee/polaroad/post/card/QueryCardRepository.java
+++ src/main/java/javaiscoffee/polaroad/post/card/QueryCardRepository.java
@@ -5,7 +5,7 @@ import javaiscoffee.polaroad.post.*;
 import java.util.List;
 
 public interface QueryCardRepository {
-    List<MapCardListDto> getMapCardListByKeyword(String searchKeyword, PostConcept concept, CardStatus status, double swLatitude, double neLatitude, double swLongitude, double neLongitude, int pageSize);
+    List<MapCardListDto> getMapCardListByKeyword(String searchKeyword, PostConcept concept, CardStatus status, PostStatus postStatus, double swLatitude, double neLatitude, double swLongitude, double neLongitude, int pageSize);
 
-    List<MapCardListDto> getMapCardListByHashtag(Long hashtagId, PostConcept concept, CardStatus status, double swLatitude, double neLatitude, double swLongitude, double neLongitude, int pageSize);
+    List<MapCardListDto> getMapCardListByHashtag(Long hashtagId, PostConcept concept, CardStatus status, PostStatus postStatus, double swLatitude, double neLatitude, double swLongitude, double neLongitude, int pageSize);
 }
